e993cd12d123a031c94e045ac0935b0d92d198a4
Md. Hasibul Hasan <50163812+hasanshanto@users.noreply.github.com>
2022-10-27 15:43:15 +0600

    file added
    coalescing two columns using case and coalesce.

---
+++ comment table.sql
@@ -0,0 +1,45 @@
+/* Write an SQL query to display the correct message (meaningful message) from the input
+comments_and_translation table. */
+
+drop table comments_;
+create table comments_
+(
+	id				int,
+	comment			varchar(100),
+	translation		varchar(100)
+);
+
+insert into comments_ values
+(1, 'very good', null),
+(2, 'good', null),
+(3, 'bad', null),
+(4, 'ordinary', null),
+(5, 'cdcdcdcd', 'very bad'),
+(6, 'excellent', null),
+(7, 'ababab', 'not satisfied'),
+(8, 'satisfied', null),
+(9, 'aabbaabb', 'extraordinary'),
+(10, 'ccddccbb', 'medium');
+
+--> Solution 1
+
+select
+case when translation is null 
+	then comment
+	else translation
+end as comments
+from comments_;
+
+
+--> Solution 2
+
+select coalesce(translation, comment) as comments
+from comments_;
+
+
+
+
+
+
+
+
